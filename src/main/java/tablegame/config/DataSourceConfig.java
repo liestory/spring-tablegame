2f8196ccc4edf5517fbeda0b7524eda7a8e18a3d
Igor Nemykin <nemykin.i.o@sberbank.ru>
2020-12-11 01:26:56 +0300

    JDBC-Template-Liquibase: init

---
+++ src/main/java/tablegame/config/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package tablegame.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * DataSourceConfig.
+ *
+ * @author nemykin 11.12.2020
+ */
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource(Environment environment) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("datasource.driver"));
+        dataSource.setUrl(environment.getRequiredProperty("datasource.url"));
+        dataSource.setUsername(environment.getRequiredProperty("datasource.username"));
+        dataSource.setPassword(environment.getRequiredProperty("datasource.password"));
+        return dataSource;
+    }
+}
---
+++ src/main/java/tablegame/config/LiquibaseConfig.java
@@ -0,0 +1,26 @@
+package tablegame.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+import javax.sql.DataSource;
+
+/**
+ * LiquibaseConfig.
+ *
+ * @author nemykin 11.12.2020
+ */
+@Configuration
+@DependsOn("dataSource")
+public class LiquibaseConfig {
+
+    @Bean
+    SpringLiquibase getSpringLiquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db/changelog/changelog-master.xml");
+        return liquibase;
+    }
+}
